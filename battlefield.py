04d32e7fa57c3751c886d92f207b46664af29f07
Akeem <akeem.mighty@gmail.com>
2022-03-23 11:32:25 -0400

    + updated isempty condition in battle function of Battlefield class + Added remove obj from list function in Battlefield class + updated show dino option and show robo option in Battlefield class + Updated dino turn and robo turn with current display option

--- battlefield.py
+++ battlefield.py
@@ -1,4 +1,3 @@
-from dinosaur import Dinosaur
 from fleet import Fleet
 from herd import Herd
 
@@ -25,8 +24,11 @@ class Battlefield:
 
     
     def battle(self):
-        if self.is_empty_group(self.fleet.robots) or self.is_empty_group(self.fleet.robots):
+        if self.is_empty_group(self.fleet.robots) or self.is_empty_group(self.herd.dinosaurs):
             self.winners = self.fleet.robots or self.herd.dinosaurs
+            self.display_winners()
+        self.show_dino_opponent_options()
+        self.show_robo_opponent_options()
         self.current_turn = None
         while True:
             user_input = int(input("Which opponent will go first? Enter 1 for Dinosaur or Enter 2 for Robot"))
@@ -52,24 +54,28 @@ class Battlefield:
     #pass in random turn
     def dino_turn(self, dinosaur):
         #select one of the dinosaur from the list
-        self.show_robo_opponent_options()
+        self.show_dino_opponent_options()
         len_of_fleet = len(self.fleet.robots)
         fleet_list = self.fleet.robots
         user_input = int(input("Which Robot would you like to attack: ")) 
         while user_input > len_of_fleet or user_input < 0:
             user_input = int(input("Which Robot would you like to attack: ")) 
         dinosaur.attack(fleet_list[user_input])
+        if fleet_list[user_input].health <= 0:
+            self.remove_obj_from_list(fleet_list[user_input], fleet_list)
         return self.battle()
         #check opponent list if empty
 
     def robo_turn(self, robot):
-        self.show_dino_opponent_options()
+        self.show_robo_opponent_options()
         len_of_herd = len(self.herd.dinosaurs)
-        dinosaur_list = self.herd.dinosaurs
+        herd_list = self.herd.dinosaurs
         user_input = int(input("Which Dinosaur would you like to attack: "))
         while user_input > len_of_herd or user_input < 0:
             user_input = int(input("Which Dinosaur would you like to attack: "))
-        robot.attack(dinosaur_list[user_input])
+        robot.attack(herd_list[user_input])
+        if herd_list[user_input].health <= 0:
+            self.remove_obj_from_list(herd_list[user_input], herd_list)
         return self.battle()
         
         
@@ -77,21 +83,24 @@ class Battlefield:
     #Create Fleet and create Herd in run game and call the show after every turn
     #show opponents after every turn
     def show_dino_opponent_options(self):
-        print("The dinosaur opponent options is as follows: ")
-        for robot in self.fleet.robots:
-            self.robot_index = self.fleet.robots.index(robot)
-            print(f"{robot.name} : [{self.robot_index}]")
-
-    def show_robo_opponent_options(self):
         print("The robot opponent options is as follows: ")
         for dinosaur in self.herd.dinosaurs:
             self.dino_index = self.herd.dinosaurs.index(dinosaur)
             print(f"{dinosaur.name} : [{self.dino_index}]")
 
+    def show_robo_opponent_options(self):
+        print("The dinosaur opponent options is as follows: ")
+        for robot in self.fleet.robots:
+            self.robot_index = self.fleet.robots.index(robot)
+            print(f"{robot.name} : [{self.robot_index}]")
+
+
+    def remove_obj_from_list(self, obj, list):
+        list.remove(obj)
+
     def display_winners(self):
+        
         print("The winners are: ")
         for winner in self.winners:
             print(winner.name)
-
-        
-
+        return 0
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from battlefield import Battlefield
 
 new_battle = Battlefield()
 new_battle.run_game()
-
+#in Battlefield class check if list index is out of range before accepting user input as valid
